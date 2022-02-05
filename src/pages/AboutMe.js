9853bd836dc2e184626b2da9a998016a0fdbc54d
ed <edheyev@gmail.com>
2022-02-05 16:59:02 +0000

    small text errors and change to adobe portfolio services

--- src/pages/AboutMe.js
+++ src/pages/AboutMe.js
@@ -3,7 +3,7 @@ import styles from "../App.module.css";
 
 const AboutMe = () => {
   const aboutMeText =
-    "My name is Ed Heywood-Everett, I am a recent University of York PhD graduate, currently living in the city of York, UK. \n Just a few of my interests include research and psychology, particularly spatial memory and the use of virtual reality in research.\n I enjoy coding; developing in javascript and c# and use MATLAB and R for data. I also love creating things both digital and not. \n \n Please explore this site to see ";
+    "My name is Ed Heywood-Everett, I am a recent University of York PhD graduate, currently living in the city of York, UK. \n Just a few of my interests include research and psychology, particularly spatial memory and the use of virtual reality in research.\n I enjoy coding; developing in javascript and c# and use MATLAB and R for data. I also love creating things both digital and not. \n \n Please explore this site to see more! ";
 
   return (
     <div>
